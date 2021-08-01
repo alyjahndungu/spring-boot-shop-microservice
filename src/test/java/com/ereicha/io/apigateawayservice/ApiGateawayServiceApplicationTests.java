e3fa6d972922340bf0ef14dcbd68f1fc7fa53e6a
elijahqode <elijah.ndungu@qhala.com>
2021-08-02 00:39:21 +0300

    initial commit

---
+++ src/test/java/com/ereicha/io/apigateawayservice/ApiGateawayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ereicha.io.apigateawayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
