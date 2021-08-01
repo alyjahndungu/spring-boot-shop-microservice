0d5eb18903847e1b8bddc659cdf914d79adbe884
elijahqode <elijah.ndungu@qhala.com>
2021-08-02 00:47:44 +0300

    first commit

---
+++ src/main/java/com/ereicha/io/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,26 @@
+package com.ereicha.io.inventoryservice.controller;
+
+import com.ereicha.io.inventoryservice.model.Inventory;
+import com.ereicha.io.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+    private final InventoryRepository inventoryRepository;
+
+    @GetMapping("/{skuCode}")
+    Boolean isInStock(@PathVariable String skuCode) {
+        log.info("Checking stock for product with skucode - " + skuCode);
+        Inventory inventory = inventoryRepository.findBySkuCode(skuCode)
+                .orElseThrow(() -> new RuntimeException("Cannot Find Product by sku code " + skuCode));
+        return inventory.getStock() > 0;
+    }
+}
